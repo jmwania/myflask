6c0a0237ce915f644772f97299c14010adf92ec1
jmwania <josphatmwania237@gmail.com>
2018-12-10 10:51:36 +0300

    author

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body':'lorem this all lies under Articles.',
+            'author': 'Josphat Mwania',
+            'create_date':'04-12-2018'
+        },
+        {
+          'id': 2,
+            'title': 'Article Two',
+            'body':'lorem this all lies under Articles.',
+            'author': 'Justin  Mwania',
+            'create_date':'04-12-2018'  
+        }
+    ]
+    return articles 
